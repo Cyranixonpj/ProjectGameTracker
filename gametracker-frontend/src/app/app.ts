430651a49a7a1589c51562c46237a4eca01d54ad
Cyranixonpj <s27730@pjwstk.edu.pl>
2026-07-18 14:41:26 +0200

    Angular

---
+++ gametracker-frontend/src/app/app.ts
@@ -0,0 +1,28 @@
+import { Component, signal, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class App implements OnInit {
+  gameData = signal<any>(null);
+
+  private http = inject(HttpClient);
+  ngOnInit() {
+
+    this.http.get('http://localhost:8080/api/games/mgs').subscribe({
+      next: (data) => {
+
+        this.gameData.set(data);
+      },
+      error: (err) => {
+        console.error('Błąd pobierania danych ze Spring Boota:', err);
+      }
+    });
+  }
+}

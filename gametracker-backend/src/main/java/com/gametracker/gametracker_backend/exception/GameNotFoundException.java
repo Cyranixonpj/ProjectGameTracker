78af89856d4c574f8907816799b466cf63c2ca62
Cyranixonpj <s27730@pjwstk.edu.pl>
2026-07-22 20:08:54 +0200

    Backend container

---
+++ gametracker-backend/src/main/java/com/gametracker/gametracker_backend/exception/GameNotFoundException.java
@@ -0,0 +1,7 @@
+package com.gametracker.gametracker_backend.exception;
+
+public class GameNotFoundException extends RuntimeException {
+    public GameNotFoundException(Long id) {
+        super("Game with id " + id + " not found.");
+    }
+}

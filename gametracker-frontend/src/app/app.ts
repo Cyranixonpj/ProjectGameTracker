08cc8f55ccc51d1462dd2d1f27f2bfd2dcfaf546
Cyranixonpj <s27730@pjwstk.edu.pl>
2026-07-23 19:51:57 +0200

    Inteelij Update + angular restart

--- gametracker-frontend/src/app/app.ts
+++ gametracker-frontend/src/app/app.ts
@@ -1,28 +1,47 @@
-import { Component, signal, inject, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import {CommonModule} from '@angular/common';
 
+
+export interface Game{
+  id: number;
+  title: string;
+  genre: string;
+  rating: number;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './app.html',
-  styleUrls: ['./app.css']
+  styleUrl: './app.css'
 })
-export class App implements OnInit {
-  gameData = signal<any>(null);
-
-  private http = inject(HttpClient);
-  ngOnInit() {
-
-    this.http.get('http://localhost:8081/api/games').subscribe({
-      next: (data) => {
-
-        this.gameData.set(data);
-      },
-      error: (err) => {
-        console.error('Błąd pobierania danych ze Spring Boota:', err);
-      }
-    });
+export class AppComponent {
+  title = 'Gametracker';
+  games: Game[] = [{
+    id: 1,
+    title: 'The Legend of Zelda: Breath of the Wild',
+    genre: 'Action-adventure',
+    rating: 10
+  },
+  {
+    id: 2,
+    title: 'Super Mario Odyssey',
+    genre: 'Platformer',
+    rating: 9
+  },
+  {
+    id: 3,
+    title: 'Red Dead Redemption 2',
+    genre: 'Action-adventure',
+    rating: 10
+  },
+  {
+    id: 4,
+    title: 'The Witcher 3: Wild Hunt',
+    genre: 'Action RPG',
+    rating: 9
   }
+
+  ]
+
 }

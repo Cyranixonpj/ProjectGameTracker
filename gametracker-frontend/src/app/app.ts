34d7910f964af40f34a3a0007491bab63acb1496
Cyranixonpj <s27730@pjwstk.edu.pl>
2026-07-27 15:01:51 +0200

    front-end container created

--- gametracker-frontend/src/app/app.ts
+++ gametracker-frontend/src/app/app.ts
@@ -1,47 +1,58 @@
-import { Component, signal } from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { Component, OnInit, inject , signal} from '@angular/core';
+import {CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+
 
 
 export interface Game{
   id: number;
   title: string;
-  genre: string;
+  developer: string;
   rating: number;
+  review: string;
 }
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class AppComponent {
-  title = 'Gametracker';
-  games: Game[] = [{
-    id: 1,
-    title: 'The Legend of Zelda: Breath of the Wild',
-    genre: 'Action-adventure',
-    rating: 10
-  },
-  {
-    id: 2,
-    title: 'Super Mario Odyssey',
-    genre: 'Platformer',
-    rating: 9
-  },
-  {
-    id: 3,
-    title: 'Red Dead Redemption 2',
-    genre: 'Action-adventure',
-    rating: 10
-  },
-  {
-    id: 4,
-    title: 'The Witcher 3: Wild Hunt',
-    genre: 'Action RPG',
-    rating: 9
+export class AppComponent implements OnInit {
+  title = 'GameTracker';
+
+  private http = inject(HttpClient);
+  games = signal<Game[]>([]);
+  averageRating = signal<number>(0);
+
+  ngOnInit(): void {
+    this.fetchGames();
+
   }
+  fetchGames(): void {
+    this.http.get<Game[]>('http://localhost:8081/api/games').subscribe({
+      next: (data) => {
+        console.log(data);
+        this.games.set(data);
+        const avg = this.calculateAverageRating(data);
+        this.averageRating.set(avg);
+      },
+      error: (err) => {
+        console.error('Błąd podczas pobierania gier', err);
+      },
 
-  ]
+    });
+  }
+  calculateAverageRating(gameList: Game[]): number{
+    if(gameList.length===0){
+      return 0
+    }
+    let sum = 0;
 
+    for (const game of gameList){
+      sum+=game.rating
+    }
+    return sum/gameList.length;
+
+  }
 }
